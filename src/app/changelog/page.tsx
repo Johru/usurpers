e6cecfe4163fdcab98bb1eb4decbebb38b498edf
Josef <johruza@gmail.com>
2026-03-25 19:51:47 +0100

    data update

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -6,11 +6,21 @@ export default function ChangelogPage() {
           Changelog
         </h1>
         <hr/>
-        <h2 className="font-bold mt-2 text-2xl">2.2 - magic overhaul</h2>
+        <h2 className="font-bold mt-2 text-2xl">2.3 - tactic and misc</h2>
+        <ul className="list-disc list-inside text-sm md:text-base">
+          <li>Merchants give bonus to Earn action</li>
+          <li>Trade Routes now give a big payout when you Earn</li>
+          <li>Entertainers give flat coins per expansion</li>
+          <li>Wraiths now place a unit when they clear a region</li>
+          <li>Flying Castles now give free outposts</li>
+          <li>Magicator lost counterspell but allows to buy charges for other cards</li>
+          <li>New tier 4 card: Blockade Patrol ﹙siege/tactic﹚</li>
+        </ul>
+          <h2 className="font-bold mt-2 text-2xl">2.2 - magic overhaul</h2>
         <ul className="list-disc list-inside text-sm md:text-base">
           <li>Alchemists damage now scales with magic</li>
           <li>Portal Hoppers replace Illusionists and teleport units into Battle</li>
-          <li>New Card: Field Magicator</li>
+          <li>New tier 5 Card: Field Magicator ﹙magic/retinue﹚</li>
         </ul>
            <h2 className="font-bold mt-2 text-2xl">2.1 - tier 5 tweaks</h2>
         <ul className="list-disc list-inside text-sm md:text-base">
